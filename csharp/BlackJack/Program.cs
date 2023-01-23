6fd878c8c6430b49466878882ab3a4cbdd5f1ee0
jennyhsl <lofthus.jenny@gmail.com>
2023-01-23 21:34:01 +0100

    soft ace

--- csharp/BlackJack/Program.cs
+++ csharp/BlackJack/Program.cs
@@ -3,35 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BlackJack
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var deck = new Deck();
-            var hand = new List<Card>();
+namespace BlackJack;
 
-            while (true)
+class Program
+{
+    static void Main(string[] args)
+    {
+        var deck = new Deck();
+        var hand = new List<Card>();
+        var softAces = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Stand, Hit");
+            string read = Console.ReadLine();
+            if (read == "Hit")
             {
-                Console.WriteLine("Stand, Hit");
-                string read = Console.ReadLine();
-                if (read == "Hit")
+                var card = deck.DrawCard();
+                hand.Add(card);
+                if (card.face == Face.A)
                 {
-                    var card = deck.DrawCard();
-                    hand.Add(card);
-                    var total = hand.Sum(x => Math.Min((int)x.face, 10));
-                    Console.WriteLine("Hit with {0} {1}. Total is {2}", card.Suit, card.face, total);
-                    if (total > 21)
-                    {
-                        Console.WriteLine("You got more than 21 points, you lost.");
-                        break;
-                    }
+                    softAces++;
                 }
-                else if (read == "Stand")
+
+                var total = hand.Sum(x => Math.Min((int)x.face, 10));
+                if (total > 21 && softAces > 0)
                 {
+                    total -= 10;
+                    softAces--;
+                }
+
+                Console.WriteLine("Hit with {0} {1}. Total is {2}", card.Suit, card.face, total);
+                if (total > 21)
+                {
+                    Console.WriteLine("You got more than 21 points, you lost.");
                     break;
                 }
+            }
+            else if (read == "Stand")
+            {
+                break;
             }
         }
     }
